a3013ea5c5f06ca2cb59076d82b4e97ff9ee2684
Ngô Gia Bảo <ngobao0419@gmail.com>
2024-10-07 22:23:14 +0700

    update home vs lich dat kham

--- src/Pages/DatKham/PhieuKhamBenh.js
+++ src/Pages/DatKham/PhieuKhamBenh.js
@@ -1,7 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import "./PhieuKhamBenh.css"; // Import file CSS
 
 const PhieuKham = () => {
+  // Tạo các state cho thông tin phiếu khám
+  const [tenBenhNhan, setTenBenhNhan] = useState("Nguyễn Văn A");
+  const [gioiTinh, setGioiTinh] = useState("Nam");
+  const [tuoi, setTuoi] = useState(24);
+  const [soDienThoai, setSoDienThoai] = useState("0923444444");
+  const [diaChi, setDiaChi] = useState("Ấp 2, Tắc Vân, TP. Cà Mau");
+  const [tenBacSi, setTenBacSi] = useState("Lê Thị Hồng");
+  const [ngayKham, setNgayKham] = useState("24/09/2024");
+  const [caKham, setCaKham] = useState(2);
+  const [sttKham, setSttKham] = useState("012");
+
   return (
     <div className="phieu-kham-container">
       <h2 className="phieu-kham-title">Phiếu khám bệnh</h2>
@@ -13,7 +24,7 @@ const PhieuKham = () => {
         </div>
         <div className="hospital-info">
             <h3>PHIẾU KHÁM BỆNH</h3>
-            <h2>STT KHÁM: 012</h2>
+            <h2>STT KHÁM: {sttKham}</h2>
         </div>
         
         <div className="barcode">
@@ -25,29 +36,29 @@ const PhieuKham = () => {
           <tbody>
             <tr>
               <td><strong>Tên bệnh nhân</strong></td>
-              <td>Nguyễn Văn A</td>
+              <td>{tenBenhNhan}</td>
               <td><strong>Giới tính</strong></td>
-              <td>Nam</td>
+              <td>{gioiTinh}</td>
             </tr>
             <tr>
               <td><strong>Tuổi</strong></td>
-              <td>24</td>
+              <td>{tuoi}</td>
               <td><strong>SĐT</strong></td>
-              <td>0923444444</td>
+              <td>{soDienThoai}</td>
             </tr>
             <tr>
               <td><strong>Địa chỉ</strong></td>
-              <td colSpan="3">Ấp 2, Tắc Vân, TP. Cà Mau</td>
+              <td colSpan="3">{diaChi}</td>
             </tr>
             <tr>
               <td><strong>Tên bác sĩ</strong></td>
-              <td colSpan="3">Lê Thị Hồng</td>
+              <td colSpan="3">{tenBacSi}</td>
             </tr>
             <tr>
               <td><strong>Ngày khám</strong></td>
-              <td>24/09/2024</td>
+              <td>{ngayKham}</td>
               <td><strong>Ca khám</strong></td>
-              <td>2</td>
+              <td>{caKham}</td>
             </tr>
           </tbody>
         </table>
--- src/Pages/TrangChu/TrangChu.js
+++ src/Pages/TrangChu/TrangChu.js
@@ -7,12 +7,10 @@ import news2 from '../../images/images-tintuc/tintuc2.png';
 import news3 from '../../images/images-tintuc/tintuc3.png';
 import news4 from '../../images/images-tintuc/tintuc4.png';
 import news5 from '../../images/images-tintuc/tintuc5.png';
-import bacsi1 from '../../images/images-bacsi/bacsi1.png'; // Đường dẫn chính xác đến ảnh
-import bacsi2 from '../../images/images-bacsi/bacsi2.png'; // Đảm bảo rằng bạn có bức ảnh này
+import bacsi1 from '../../images/images-bacsi/bacsi1.png'; 
+import bacsi2 from '../../images/images-bacsi/bacsi2.png'; 
 import bacsi3 from '../../images/images-bacsi/bacsi3.png';
 import 'font-awesome/css/font-awesome.min.css';
-
-
 
 const newsImages = [news1, news2, news3, news4, news5];
 
@@ -46,6 +44,15 @@ const TrangChu = () => {
     setCurrentSlide((prev) => (prev - 1 + (newsImages.length - slidesToShow + 1)) % (newsImages.length - slidesToShow + 1));
   };
 
+  // Auto slide effect
+  useEffect(() => {
+    const interval = setInterval(() => {
+      nextSlide();
+    }, 3000); // Change slide every 3 seconds
+
+    return () => clearInterval(interval); // Clear interval on component unmount
+  }, []);
+
   return (
     <div>
       <Snackbar
@@ -59,11 +66,10 @@ const TrangChu = () => {
         </MuiAlert>
       </Snackbar>
       <div className="home-container">
-        {/* Video in the center */}
         <div className="video-container">
           <iframe
-            width="80%"
-            height="500px"
+            width="100%"
+            height="550px"
             src="https://www.youtube.com/embed/bvjyd7wVIGQ"
             title="YouTube video player"
             frameBorder="0"
@@ -71,93 +77,108 @@ const TrangChu = () => {
             allowFullScreen>
           </iframe>
         </div>
+        <h2 style={{ textAlign: "center", color: "#22668E" }}>Tin tức mới</h2>
 
         {/* News slider */}
         <div className="news-slider">
-      <button className="slider-button" onClick={prevSlide}>&lt;</button> {/* Left Arrow */}
-      <div className="slide">
-        {newsImages.slice(currentSlide, currentSlide + slidesToShow).map((image, index) => {
-          const links = [
-            "https://lucnam.bacgiang.gov.vn/chi-tiet-tin-tuc/-/asset_publisher/Enp27vgshTez/content/chu-ong-trien-khai-cac-hoat-ong-phong-chong-dich-benh/20181",
-            "https://tuyengiao.hungyen.dcs.vn/tang-cuong-phong-chong-dich-covid-19-bang-y-hoc-co-truyen-c28332.html/",
-            "https://baobinhphuoc.com.vn/news/14/143498/tang-cuong-tiem-vaccine-phong-covid-19-uu-tien-bao-ve-nhom-nguy-co-cao/",
-            "https://dienbientv.vn/tin-tuc-su-kien/y-te-suc-khoe/202108/mot-so-trieu-chung-phat-ban-co-the-la-dau-hieu-mac-covid-19-5747096/",
-            "https://baophapluat.vn/13-hoc-sinh-nhap-vien-sau-khi-uong-nuoc-ngot-mien-phi-o-cong-truong-post527209.html"
-          ];
-
-          return (
-            <a key={index} href={links[index % links.length]} target="_blank" rel="noopener noreferrer">
-              <img src={image} alt={`News ${currentSlide + index + 1}`} />
-            </a>
-          );
-        })}
-      </div>
-      <button className="slider-button" onClick={nextSlide}>&gt;</button> {/* Right Arrow */}
-    </div>
-
-
-      {/* 3. Đoạn chữ với background màu xanh dương nhạt */}
-      <div className="text-section">
-        <h2>Chúng tôi có sự khác biệt</h2>
-        <p>Phòng khám UMC là nơi bạn có thể tin tưởng với đội ngũ bác sĩ giàu kinh nghiệm, trang thiết bị hiện đại, và dịch vụ chăm sóc tận tâm. 
-          Chúng tôi luôn đặt sức khỏe của bạn lên hàng đầu, cam kết mang đến những giải pháp y tế toàn diện, an toàn và hiệu quả.
-           Với sự chuyên nghiệp trong quá trình chẩn đoán và điều trị, Phòng khám UMC không chỉ mang đến cho bạn sự an tâm mà còn là sự hài lòng về 
-           chất lượng dịch vụ. Hãy đến với chúng tôi để trải nghiệm môi trường y tế chuẩn quốc tế, nơi mỗi bệnh nhân đều được chăm sóc như người thân
-            trong gia đình.
-           </p>
-      </div>
-
-      {/* 4. Đội ngũ bác sĩ gồm 3 ảnh */}
-      <div className="doctor">
-        <h2>Đội ngũ Y - Bác sĩ ưu tú</h2>
-      <div className="doctor-team">
-        
-        <img src={bacsi1} alt="Doctor 1" />
-        <img src={bacsi2} alt="Doctor 2" />
-        <img src={bacsi3} alt="Doctor 3" />
+          <button className="slider-button" onClick={prevSlide}>&lt;</button> {/* Left Arrow */}
+          <div className="slide">
+            {newsImages.slice(currentSlide, currentSlide + slidesToShow).map((image, index) => {
+              const links = [
+                "https://lucnam.bacgiang.gov.vn/chi-tiet-tin-tuc/-/asset_publisher/Enp27vgshTez/content/chu-ong-trien-khai-cac-hoat-ong-phong-chong-dich-benh/20181",
+                "https://tuyengiao.hungyen.dcs.vn/tang-cuong-phong-chong-dich-covid-19-bang-y-hoc-co-truyen-c28332.html/",
+                "https://baobinhphuoc.com.vn/news/14/143498/tang-cuong-tiem-vaccine-phong-covid-19-uu-tien-bao-ve-nhom-nguy-co-cao/",
+                "https://dienbientv.vn/tin-tuc-su-kien/y-te-suc-khoe/202108/mot-so-trieu-chung-phat-ban-co-the-la-dau-hieu-mac-covid-19-5747096/",
+                "https://baophapluat.vn/13-hoc-sinh-nhap-vien-sau-khi-uong-nuoc-ngot-mien-phi-o-cong-truong-post527209.html"
+              ];
+              return (
+                <a key={index} href={links[index % links.length]} target="_blank" rel="noopener noreferrer">
+                  <img src={image} alt={`News ${currentSlide + index + 1}`} />
+                </a>
+              );
+            })}
+          </div>
+          <button className="slider-button" onClick={nextSlide}>&gt;</button> {/* Right Arrow */}
         </div>
-        <div className="doctor-buttons">
-        <button className="schedule-button">Đặt lịch hẹn ngay</button>
-        <button className="details-button">Xem chi tiết</button>
-      </div>
-      </div>
 
-      {/* 5. Số hotline và button tìm hiểu thêm */}
-      <div className="hotline-section">
-        <h2>PHÒNG KHÁM THEO YÊU CẦU</h2>
-        <p>Bạn không có thời gian? Hãy gọi ngay Hotline Phòng khám umc để đặt lịch khám dịch vụ nhanh với các bác sĩ, chuyên gia của chúng tôi</p>
-        <h2>Hotline: 0962 240 221</h2>
-        <button>Tìm hiểu thêm</button>
-      </div>
-
-      {/* 6. Góp ý liên hệ */}
-      <div className="feedback-section">
-          <h3>Góp ý liên hệ</h3>
-          <p>
-            Nếu quý khách có góp ý, thắc mắc hay ý kiến phản hồi, đóng góp xin vui lòng điền vào 
-            Mẫu bên phải và gửi cho chúng tôi. Xin chân thành cảm ơn!
+        {/* 3. Đoạn chữ với background màu xanh dương nhạt */}
+        <div className="text-section">
+          <h2>CHÚNG TÔI CÓ SỰ KHÁC BIỆT</h2>
+          <p>Phòng khám UMC là nơi bạn có thể tin tưởng với đội ngũ bác sĩ giàu kinh nghiệm, trang thiết bị hiện đại, và dịch vụ chăm sóc tận tâm. 
+            Chúng tôi luôn đặt sức khỏe của bạn lên hàng đầu, cam kết mang đến những giải pháp y tế toàn diện, an toàn và hiệu quả.
+            Với sự chuyên nghiệp trong quá trình chẩn đoán và điều trị, Phòng khám UMC không chỉ mang đến cho bạn sự an tâm mà còn là sự hài lòng về 
+            chất lượng dịch vụ. Hãy đến với chúng tôi để trải nghiệm môi trường y tế chuẩn quốc tế, nơi mỗi bệnh nhân đều được chăm sóc như người thân
+            trong gia đình.
           </p>
-          <form>
-  <div className="form-group">
-    <input className="input-field" type="text" placeholder="Họ tên" required />
-    <input className="input-field" type="email" placeholder="Email" required />
-  </div>
-  <div className="form-group">
-    <input className="input-field" type="text" placeholder="Tiêu đề" required />
-    <input className="input-field" type="tel" placeholder="Số điện thoại" required />
-  </div>
-  <div className="form-group">
-    <textarea className="textarea-field" placeholder="Nội dung liên hệ" required></textarea>
-  </div>
-  <button type="submit">Gửi phản hồi</button>
-</form>
+        </div>
 
+   {/* 4. Đội ngũ bác sĩ gồm 3 ảnh */}
+        <div className="doctor">
+          <h2>ĐỘI NGŨ Y - BÁC SĨ CHUYÊN NGHIỆP</h2>
+          <div className="doctor-team">
+            <div className="doctor-item">
+              <img src={bacsi1} alt="Doctor 1" />
+              <p>BS Chà My</p>
+            </div>
+            <div className="doctor-item">
+              <img src={bacsi2} alt="Doctor 2" />
+              <p>BS TUẤN</p>
+            </div>
+            <div className="doctor-item">
+              <img src={bacsi3} alt="Doctor 3" />
+              <p>BS THỊNH</p>
+            </div>
+          </div>
+          <div className="doctor-buttons">
+            <button className="schedule-button">Đặt lịch hẹn ngay</button>
+            <button className="details-button">Xem chi tiết</button>
+          </div>
+        </div>
+
+
+        {/* 5. Số hotline và button tìm hiểu thêm */}
+        <div className="hotline-section">
+          <h2>PHÒNG KHÁM THEO YÊU CẦU</h2>
+          <p>Bạn không có thời gian? Hãy gọi ngay Hotline Phòng khám umc để đặt lịch khám dịch vụ nhanh với các bác sĩ, chuyên gia của chúng tôi</p>
+          <h2>Hotline: 0962 240 221</h2>
+          <button>Tìm hiểu thêm</button>
+        </div>
+
+        {/* 6. Góp ý liên hệ */}
+        <div className="feedback-section">
+          <h2>LIÊN HỆ VỚI CHÚNG TÔI</h2>
+        
+          <div className="feedback-container">
+            <div className="feedback-info">
+              <p> Nếu quý khách có góp ý, thắc mắc hay ý kiến phản hồi, đóng góp xin vui lòng điền vào
+              Mẫu bên phải và gửi cho chúng tôi. Xin chân thành cảm ơn!</p>
+            </div>
+
+            <div className="feedback-form">
+              <form>
+                <div className="form-group">
+                  <input className="input-field" type="text" placeholder="Họ tên" required />
+                  <input className="input-field" type="email" placeholder="Email" required />
+                </div>
+                <div className="form-group">
+                  <input className="input-field" type="text" placeholder="Tiêu đề" required />
+                  <input className="input-field" type="tel" placeholder="Số điện thoại" required />
+                </div>
+                <div className="form-group">
+                  <textarea className="textarea-field" placeholder="Nội dung liên hệ" required></textarea>
+                </div>
+                <div className="button-container">
+                  <button type="submit">Gửi phản hồi</button>
+                </div>
+              </form>
+            </div>
+        </div>
       </div>
 
+       
 
-
-      {/* 7. Footer */}
-      <footer>
+        {/* 7. Footer */}
+        <footer>
             <div className="footer-left">
               <h4>Công ty cổ phần trung tâm y khoa UMC</h4>
               <p>
