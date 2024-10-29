5a929357e03671b7eb94b5e34cace59f82b6b59d
C3 <thvinhhtran29@gmail.com>
2024-10-29 13:05:24 +0700

    fix:tach footer ra khoi trang chu

--- src/Pages/TrangChu/TrangChu.js
+++ src/Pages/TrangChu/TrangChu.js
@@ -11,7 +11,7 @@ import bacsi1 from "../../images/images-bacsi/bacsi1.png";
 import bacsi2 from "../../images/images-bacsi/bacsi2.png";
 import bacsi3 from "../../images/images-bacsi/bacsi3.png";
 import "font-awesome/css/font-awesome.min.css";
-
+import Footer from "./Footer";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/autoplay";
@@ -346,44 +346,7 @@ const TrangChu = () => {
                     </div>
                 </div>
                 {/* 7. Footer */}
-                <footer>
-                    <div className="footer-trangchu-left">
-                        <h4>Công ty cổ phần trung tâm y khoa UMC</h4>
-                        <p>
-                            <i className="fas fa-map-marker-alt"></i> 201 Nguyễn Chí Thanh, Phường 12, Quận 5, Thành phố Hồ Chí Minh.
-                        </p>
-                        <p>
-                            <i className="fas fa-phone"></i> Đường dây nóng: 0962 240 221
-                        </p>
-                        <p>
-                            <i className="fas fa-headset"></i> Tổng đài tư vấn: (028) 3015 9457
-                        </p>
-                        <p>
-                            <i className="fas fa-fax"></i> Fax: (028) 3815 9465
-                        </p>
-                        <p>
-                            <i className="fas fa-envelope"></i> umc@bbnnaa.com
-                        </p>
-                    </div>
-                    <div className="footer-trangchu-right">
-                        <h4>Lịch làm việc</h4>
-                        <p>
-                            <i className="fas fa-clock"></i> Thứ 2 - Thứ 7 | 8:00 - 17:00
-                        </p>
-                        <p>
-                            <i className="fas fa-child"></i> Khám Nhi, Xét nghiệm Nhi & Xét nghiệm nhanh Covid
-                        </p>
-                        <p>
-                            <i className="fas fa-sun"></i> Chủ nhật | 8:00 - 12:00
-                        </p>
-                        <p>
-                            <i className="fas fa-lunchbox"></i> Khám Nhi, Xét nghiệm Nhi & Xét nghiệm nhanh Covid
-                        </p>
-                        <p>
-                            <i className="fas fa-clock"></i> Giờ nghỉ trưa: 12:00 - 13:00
-                        </p>
-                    </div>
-                </footer>
+                <Footer />
             </div>
         </div>
     );
